25350137f0a620b9c071cea8012ae6254a2c79f2
Ayan-droid7 <ayanrahman271@gmail.com>
2026-08-28 23:42:58 +0530

    Time: 0 ms (100%), Space: 19.7 MB (87.82%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def thirdMax(self, nums: list[int]) -> int:
+        first = second = third = None
+        
+        for num in nums:
+            # Skip duplicates of already tracked maximums
+            if num in (first, second, third):
+                continue
+            
+            if first is None or num > first:
+                first, second, third = num, first, second
+            elif second is None or num > second:
+                second, third = num, second
+            elif third is None or num > third:
+                third = num
+                
+        # If third maximum doesn't exist, return the overall maximum (first)
+        return third if third is not None else first
